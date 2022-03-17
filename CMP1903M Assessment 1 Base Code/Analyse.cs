e176ff7013d00be59ed41a8fac107d7430d6d3b1
Lynx <lynxat0mic02@gmail.com>
2022-03-17 13:55:47 +0000

    Implemented analysis functionality

--- CMP1903M Assessment 1 Base Code/Analyse.cs
+++ CMP1903M Assessment 1 Base Code/Analyse.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CMP1903M_Assessment_1_Base_Code
@@ -14,7 +15,7 @@ namespace CMP1903M_Assessment_1_Base_Code
         //Arguments: string
         //Returns: list of integers
         //Calculates and returns an analysis of the text
-        public List<int> analyseText(string input)
+        public Dictionary<string, int> analyseText(string input)
         {
             //List of integers to hold the first five measurements:
             //1. Number of sentences
@@ -22,15 +23,37 @@ namespace CMP1903M_Assessment_1_Base_Code
             //3. Number of consonants
             //4. Number of upper case letters
             //5. Number of lower case letters
-            List<int> values = new List<int>();
             //Initialise all the values in the list to '0'
-            for(int i = 0; i<5; i++)
+            var values = new Dictionary<string, int>();
+            var tools = new Dictionary<string, Func<string, int>>()
             {
-                values.Add(0);
+                {"sentences", s => Regex.Match(s, "/(\\s|^)+[^.!?\\n]*[.!?\\n]/gm").Length},
+                {"vowels", s => Regex.Match(s, "/[aeiou]/gm").Length},
+                {"consonants", s => Regex.Match(s, "/[^aeiou]/gm").Length},
+                {"upper", s => Regex.Match(s, "/[A-Z]/gm").Length},
+                {"lower", s => Regex.Match(s, "/[a-z]/gm").Length}
+            };
+
+            foreach (var toolsKey in tools.Keys)
+            {
+                values.Add(toolsKey, new AnalysisTool(tools[toolsKey]).Count(input));
             }
 
-
             return values;
+        }
+    }
+    
+    public class AnalysisTool
+    {
+        private Func<string, int> tool;
+        public AnalysisTool(Func<string, int> tool)
+        {
+            this.tool = tool;
+        }
+
+        public int Count(string text)
+        {
+            return this.tool(text);
         }
     }
 }
