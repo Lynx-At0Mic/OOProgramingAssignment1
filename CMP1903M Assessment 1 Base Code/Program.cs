165cea68e180e5f2f97017a97d5645a5d85eff64
Lynx <lynxat0mic02@gmail.com>
2022-03-21 14:53:45 +0000

    Added main program routine

--- CMP1903M Assessment 1 Base Code/Program.cs
+++ CMP1903M Assessment 1 Base Code/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace CMP1903M_Assessment_1_Base_Code
@@ -12,28 +13,51 @@ namespace CMP1903M_Assessment_1_Base_Code
         static void Main()
         {
             //Local list of integers to hold the first five measurements of the text
-            List<int> parameters = new List<int>();
+            // List<int> parameters = new List<int>();
 
             //Create 'Input' object
             //Get either manually entered text, or text from a file
+            Input ioReader = new Input();
+            string testString = "";
+            
+            while (true)
+            {
+                Console.WriteLine("Options:\n1: Manual input\n2: File input\n3: Exit");
+                string usrInput = Console.ReadLine();
+                if (usrInput == "1")
+                {
+                    Console.WriteLine("Enter text");
+                    testString = ioReader.manualTextInput();
+                }
+                else if (usrInput == "2")
+                {
+                    Console.WriteLine("Enter absolute file path");
+                    testString = ioReader.fileTextInput(Console.ReadLine());
+                }
+                else if (usrInput == "3") break;
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+                
+                //Create an 'Analyse' object
+                //Pass the text input to the 'analyseText' method
+                //Receive a list of integers back
+                var dict = Analyse.AnalyseText(testString);
 
+                //Report the results of the analysis
+                Console.WriteLine("\n");
+                foreach (var key in dict.Keys)
+                {
+                    Console.WriteLine("{0}:   \t{1}", key, dict[key]);
+                }
 
-            //Create an 'Analyse' object
-            //Pass the text input to the 'analyseText' method
-
-
-            //Receive a list of integers back
-
-
-            //Report the results of the analysis
-
-
-            //TO ADD: Get the frequency of individual letters?
-
-           
+                Console.WriteLine("\n");
+                
+                //TO ADD: Get the frequency of individual letters?
+                
+            }
         }
-        
-        
-    
     }
 }
