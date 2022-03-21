5d317975b561625b8a535e44702d400cfee476b8
Lynx <lynxat0mic02@gmail.com>
2022-03-21 14:39:28 +0000

    Added input class

--- CMP1903M Assessment 1 Base Code/Input.cs
+++ CMP1903M Assessment 1 Base Code/Input.cs
@@ -9,7 +9,7 @@ namespace CMP1903M_Assessment_1_Base_Code
     public class Input
     {
         //Handles the text input for Assessment 1
-        string text = "nothing";
+        string text = "";
         
         //Method: manualTextInput
         //Arguments: none
@@ -17,7 +17,8 @@ namespace CMP1903M_Assessment_1_Base_Code
         //Gets text input from the keyboard
         public string manualTextInput()
         {
-
+            text = "";
+            this.text = Console.ReadLine();
             return text;
         }
 
@@ -27,7 +28,11 @@ namespace CMP1903M_Assessment_1_Base_Code
         //Gets text input from a .txt file
         public string fileTextInput(string fileName)
         {
-
+            this.text = "";
+            using (var sr = new StreamReader(fileName))
+            {
+                text = sr.ReadToEnd();
+            }
             return text;
         }
 
