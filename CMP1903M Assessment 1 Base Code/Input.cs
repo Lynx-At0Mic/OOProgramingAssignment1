fcf3dc76e2d3600ee4016f434dbcf37fca551749
Lynx <lynxat0mic02@gmail.com>
2022-03-24 10:46:04 +0000

    Refactored Input.cs

--- CMP1903M Assessment 1 Base Code/Input.cs
+++ CMP1903M Assessment 1 Base Code/Input.cs
@@ -1,40 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CMP1903M_Assessment_1_Base_Code
 {
+    /// <summary>
+    /// Used to accept both manual text input and text from a file
+    /// </summary>
     public class Input
     {
-        //Handles the text input for Assessment 1
-        string text = "";
-        
-        //Method: manualTextInput
-        //Arguments: none
-        //Returns: string
-        //Gets text input from the keyboard
+        /// <summary>
+        /// Used to accept text input through the command line
+        /// </summary>
+        /// <returns>string: Text input</returns>
         public string manualTextInput()
         {
-            text = "";
-            this.text = Console.ReadLine();
-            return text;
-        }
-
-        //Method: fileTextInput
-        //Arguments: string (the file path)
-        //Returns: string
-        //Gets text input from a .txt file
-        public string fileTextInput(string fileName)
-        {
-            this.text = "";
-            using (var sr = new StreamReader(fileName))
+            Console.WriteLine("Enter text:");
+            while (true)
             {
-                text = sr.ReadToEnd();
+                try
+                {
+                    return Console.ReadLine() ?? string.Empty;
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("An IO error occured, assuming user entered nothing");
+                    return string.Empty;
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine("Line is too long! Try again");
+                }
             }
-            return text;
         }
+        
+        /// <summary>
+        /// Used to get filepath from user and read contents
+        /// </summary>
+        /// <returns>string: File contents</returns>
+        public string fileTextInput()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the filepath of the text file:");
+                string fileName = Console.ReadLine() ?? string.Empty;
 
+                try
+                {
+                    // Create StreamReader to read file
+                    using (var sr = new StreamReader(fileName))
+                    {
+                        return sr.ReadToEnd();
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Error, could not read the file!");
+                }
+            }
+        }
     }
 }
