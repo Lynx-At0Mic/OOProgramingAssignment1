fd5488a2b423dddab75777df38bdd37c2e390748
Lynx <lynxat0mic02@gmail.com>
2022-03-24 10:45:51 +0000

    Refactored Analyse.cs

--- CMP1903M Assessment 1 Base Code/Analyse.cs
+++ CMP1903M Assessment 1 Base Code/Analyse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -7,15 +8,46 @@ using System.Threading.Tasks;
 
 namespace CMP1903M_Assessment_1_Base_Code
 {
-    public static class Analyse
+    /// <summary>
+    /// Analyses text
+    /// </summary>
+    public class Analyse
     {
-        //Handles the analysis of text
+        // Text to analyse, runs AnalyseText method when set
+        private string _analysisText;
+        public string AnalisisText
+        {
+            get { return _analysisText; }
+            set
+            {
+                _analysisText = value;
+                AnalyseText(_analysisText);
+            }
+        }
 
-        //Method: analyseText
-        //Arguments: string
-        //Returns: list of integers
-        //Calculates and returns an analysis of the text
-        public static Dictionary<string, int> AnalyseText(string input)
+        public Dictionary<string, int> Elements { get; private set; }
+        public Dictionary<string, int> Characters { get; private set; }
+
+        public Analyse()
+        {
+            
+        }
+        /// <summary>
+        /// Runs analysis methods
+        /// </summary>
+        /// <param name="input">Text to analyse</param>
+        protected void AnalyseText(string input)
+        {
+            Elements = CountElements(input);
+            Characters = CountChars(input);
+        }
+
+        /// <summary>
+        /// Analyses text using instances of AnalysisTool
+        /// </summary>
+        /// <param name="text">Text to analyse</param>
+        /// <returns>dict: Keys as text elements and values corresponding to count</returns>
+        protected Dictionary<string, int> CountElements(string text)
         {
             //List of integers to hold the first five measurements:
             //1. Number of sentences
@@ -23,7 +55,8 @@ namespace CMP1903M_Assessment_1_Base_Code
             //3. Number of consonants
             //4. Number of upper case letters
             //5. Number of lower case letters
-            //Initialise all the values in the list to '0'
+            
+            // Create dictionary with anonymous functions that count elements using Regex
             var tools = new Dictionary<string, Func<string, int>>()
             {
                 {"sentences", s => Regex.Matches(s, @"(\s|^)+[^.!?]*([.!?]|\S)", RegexOptions.Multiline).Count()},
@@ -32,23 +65,55 @@ namespace CMP1903M_Assessment_1_Base_Code
                 {"upper", s => Regex.Matches(s, @"[A-Z]", RegexOptions.Multiline).Count()},
                 {"lower", s => Regex.Matches(s, @"[a-z]", RegexOptions.Multiline).Count()}
             };
+            
+            // Return a dictionary. Use Linq to create a dictionary with elements as keys and returned values from
+            // instances of AnalysisTool as values
+            return tools.Keys.ToDictionary(toolsKey => toolsKey, toolsKey => new AnalysisTool(tools[toolsKey]).Count(text));
+        }
 
-            return tools.Keys.ToDictionary(toolsKey => toolsKey, toolsKey => new AnalysisTool(tools[toolsKey]).Count(input));
+        protected Dictionary<string, int> CountChars(string text)
+        {
+            var dict = new Dictionary<string, int>();
+            foreach (Match match in Regex.Matches(text.ToLower(), @"[\w\S]"))
+            {
+                if (!dict.ContainsKey(match.ToString()))
+                {
+                    dict.Add(match.ToString(), 1);
+                }
+                else
+                {
+                    dict[match.ToString()] += 1;
+                }
+            }
+
+            return dict;
         }
     }
     
+    /// <summary>
+    /// Utility class used to analyse text
+    /// </summary>
     public class AnalysisTool
     {
+        // Readonly Func used for analysing text
         private readonly Func<string, int> _tool;
+        
+        /// <summary>
+        /// Sets up tool
+        /// </summary>
+        /// <param name="tool">Function used to analyse text</param>
         public AnalysisTool(Func<string, int> tool)
         {
-            this._tool = tool;
+            _tool = tool;
         }
-
+        /// <summary>
+        /// Uses _tool to analyse text
+        /// </summary>
+        /// <param name="text">Text to analyse</param>
+        /// <returns>int: Number of occurrences in text</returns>
         public int Count(string text)
         {
-            // Console.WriteLine(this._tool(text));
-            return this._tool(text);
+            return _tool(text);
         }
     }
 }
