9a0f4000395485e5df5c38d40d4b19490176d61b
Lynx-At0Mic <lynxat0mic02@gmail.com>
2022-03-24 21:04:07 +0000

    Fixed comments

--- CMP1903M Assessment 1 Base Code/Report.cs
+++ CMP1903M Assessment 1 Base Code/Report.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace CMP1903M_Assessment_1_Base_Code
 {
     /// <summary>
-    /// Used to generate reports and format them
+    /// Used to generate reports and format them, example of abstraction
     /// </summary>
     public class Report
     {
@@ -66,7 +66,7 @@ namespace CMP1903M_Assessment_1_Base_Code
 }
 
 /// <summary>
-/// Utility class used to generate table rows
+/// Utility class used to generate table rows, example of abstraction
 /// </summary>
 public class DataTableRow
 {
