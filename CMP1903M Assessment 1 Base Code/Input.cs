d11d19a0ffa0b103db193204aef105ada01380eb
Lynx <lynxat0mic02@gmail.com>
2022-03-28 14:31:35 +0100

    Changed read method

--- CMP1903M Assessment 1 Base Code/Input.cs
+++ CMP1903M Assessment 1 Base Code/Input.cs
@@ -57,7 +57,7 @@ namespace CMP1903M_Assessment_1_Base_Code
                 try
                 {
                     // Create StreamReader to read file
-                    return File.ReadAllLines(fileName).Trim('*');
+                    return File.ReadAllText(fileName).Trim('*');
                 }
                 catch (IOException e)
                 {
