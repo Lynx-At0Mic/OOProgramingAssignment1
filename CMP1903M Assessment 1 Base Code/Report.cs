356dc844451c07555c7fa8117a0a46a4b9c27a59
Lynx-At0Mic <lynxat0mic02@gmail.com>
2022-03-30 23:20:05 +0100

    Added file writing for long words

--- CMP1903M Assessment 1 Base Code/Report.cs
+++ CMP1903M Assessment 1 Base Code/Report.cs
@@ -32,6 +32,44 @@ namespace CMP1903M_Assessment_1_Base_Code
             }
 
             Console.WriteLine("\n");
+            if (!WriteLongWordsFile())
+            {
+                Console.WriteLine("Could not write long words file! Something is wrong!");
+            }
+        }
+
+        /// <summary>
+        /// Creates text file containing long words found in text
+        /// </summary>
+        /// <returns>bool: True if successful</returns>
+        public bool WriteLongWordsFile()
+        {
+            string longWordsFilePath = "./../../../long_words.txt";
+            if (File.Exists(longWordsFilePath)) // Check if file exists
+            {
+                File.Delete(longWordsFilePath);
+            }
+
+            // Write words to file
+            try
+            {
+                using StreamWriter sw = File.CreateText(longWordsFilePath);
+                foreach (string word in _data.LongWords)
+                {
+                    sw.WriteLine(word);
+                }
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                return false;
+            }
+            catch (PathTooLongException e)
+            {
+                return false;
+            }
+
+            return true;
+
         }
     }
 }
